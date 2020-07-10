9ef444d5c547da488ad30d8cbb60c70c740b53a7
kosta <kosta.lazarevski89@gmail.com>
2020-07-10 21:35:37 +0200

    models creation

---
+++ src/main/java/com/demo/fleetapp/Country.java
@@ -0,0 +1,18 @@
+package com.demo.fleetapp;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, )
+public class Country {
+
+}
