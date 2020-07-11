df06b23d1d2da03ce19dea582c5459afea595460
kosta <kosta.lazarevski89@gmail.com>
2020-07-11 11:24:23 +0200

    Models added

--- src/main/java/com/demo/fleetapp/Country.java
+++ src/main/java/com/demo/fleetapp/Country.java
@@ -7,12 +7,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, )
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String code;
+    private String capital;
+    private String nationality;
+    private String continent;
+    private String description;
 }
