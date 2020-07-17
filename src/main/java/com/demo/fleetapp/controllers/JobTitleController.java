fa9c757bcb4ac12b3c1325efce6ac058ea8f5bc3
kosta <kosta.lazarevski89@gmail.com>
2020-07-17 15:52:45 +0200

    Linking html pages

--- src/main/java/com/demo/fleetapp/controllers/JobTitleController.java
+++ src/main/java/com/demo/fleetapp/controllers/JobTitleController.java
@@ -1,7 +1,12 @@
 package com.demo.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JobTitleController {
+    @GetMapping("/job_title")
+    public String getJobTitles(){
+        return "job_title";
+    }
 }
