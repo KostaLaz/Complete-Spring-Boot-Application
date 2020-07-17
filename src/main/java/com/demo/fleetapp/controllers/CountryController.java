4750dc80bd16733065972f079f43bae0251fbad0
kosta <kosta.lazarevski89@gmail.com>
2020-07-17 13:27:06 +0200

    Linking html pages

--- src/main/java/com/demo/fleetapp/controllers/CountryController.java
+++ src/main/java/com/demo/fleetapp/controllers/CountryController.java
@@ -1,7 +1,12 @@
 package com.demo.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CountryController {
+    @GetMapping("/index")
+    public String goHome(){
+        return "country";
+    }
 }
