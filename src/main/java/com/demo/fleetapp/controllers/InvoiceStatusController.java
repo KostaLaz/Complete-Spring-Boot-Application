bb2a384e9de3a3fba71218a220ac278df5591465
kosta <kosta.lazarevski89@gmail.com>
2020-07-17 15:42:19 +0200

    Linking htmal pages

--- src/main/java/com/demo/fleetapp/controllers/InvoiceStatusController.java
+++ src/main/java/com/demo/fleetapp/controllers/InvoiceStatusController.java
@@ -1,7 +1,12 @@
 package com.demo.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InvoiceStatusController {
+    @GetMapping("/invoice_status")
+    public String getInvoiceStatus(){
+        return "invoice_status";
+    }
 }
