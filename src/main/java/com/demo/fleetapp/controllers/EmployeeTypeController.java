e3fac0a0d8be3a4e6327bc1395403a05f6d82054
kosta <kosta.lazarevski89@gmail.com>
2020-07-17 13:48:39 +0200

    Linking html pages

--- src/main/java/com/demo/fleetapp/controllers/EmployeeTypeController.java
+++ src/main/java/com/demo/fleetapp/controllers/EmployeeTypeController.java
@@ -1,7 +1,12 @@
 package com.demo.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeTypeController {
+    @GetMapping("employeetype")
+    public String getEmployeeType(){
+        return "employee_type";
+    }
 }
