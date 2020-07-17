4d363f11a66607297b3a2f1332c1668554a036cf
kosta <kosta.lazarevski89@gmail.com>
2020-07-17 16:03:12 +0200

    Linking html pages

--- src/main/java/com/demo/fleetapp/controllers/VehicleController.java
+++ src/main/java/com/demo/fleetapp/controllers/VehicleController.java
@@ -1,7 +1,12 @@
 package com.demo.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehicleController {
+    @GetMapping("/vehicle")
+    public String getUser(){
+        return "vehicle";
+    }
 }
--- src/main/java/com/demo/fleetapp/controllers/VehicleTypeController.java
+++ src/main/java/com/demo/fleetapp/controllers/VehicleTypeController.java
@@ -1,7 +1,12 @@
 package com.demo.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehicleTypeController {
+    @GetMapping("/user")
+    public String getUser(){
+        return "user";
+    }
 }
