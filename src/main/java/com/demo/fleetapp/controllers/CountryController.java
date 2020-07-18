0cfefa8be5965b368088e3cce851cc5b3a60d35b
kosta <kosta.lazarevski89@gmail.com>
2020-07-18 20:05:27 +0200

    Displaying data in html pages

--- src/main/java/com/demo/fleetapp/controllers/CountryController.java
+++ src/main/java/com/demo/fleetapp/controllers/CountryController.java
@@ -1,12 +1,28 @@
 package com.demo.fleetapp.controllers;
 
+import com.demo.fleetapp.model.Country;
+import com.demo.fleetapp.service.CountryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
+
+    private CountryService countryService;
+
     @GetMapping("/countries")
     public String getCountries(){
         return "country";
     }
+
+    @GetMapping("/allcountries")
+        public List<Country> getAllCountries(Model model){
+        List<Country> countryList = countryService.getAllCountries();
+        model.addAttribute("countries", countryList);
+        return countryList;
+    }
+
 }
--- src/main/java/com/demo/fleetapp/service/CountryService.java
+++ src/main/java/com/demo/fleetapp/service/CountryService.java
@@ -1,7 +1,17 @@
 package com.demo.fleetapp.service;
 
+import com.demo.fleetapp.model.Country;
+import com.demo.fleetapp.repository.CountryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    private CountryRepository countryRepository;
+
+    public List<Country> getAllCountries(){
+        return countryRepository.findAll();
+    }
 }
