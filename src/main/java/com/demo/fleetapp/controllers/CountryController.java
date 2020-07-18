780848545d85a43f205af0737d4f9f05eec4f124
kosta <kosta.lazarevski89@gmail.com>
2020-07-18 19:10:01 +0200

    Navigation structure and controllers changes

--- src/main/java/com/demo/fleetapp/controllers/CountryController.java
+++ src/main/java/com/demo/fleetapp/controllers/CountryController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CountryController {
-    @GetMapping("/country")
+    @GetMapping("/countries")
     public String getCountries(){
         return "country";
     }
