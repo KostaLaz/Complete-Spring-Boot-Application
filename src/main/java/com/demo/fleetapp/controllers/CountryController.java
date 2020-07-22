8bbda75116347373fa33409dd011973c2706a93d
kosta <kosta.lazarevski89@gmail.com>
2020-07-22 14:05:06 +0200

    Country controller fix

--- src/main/java/com/demo/fleetapp/controllers/CountryController.java
+++ src/main/java/com/demo/fleetapp/controllers/CountryController.java
@@ -1,7 +1,7 @@
 package com.demo.fleetapp.controllers;
 
 import com.demo.fleetapp.model.Country;
-import com.demo.fleetapp.service.CountryService;
+import com.demo.fleetapp.service.CountryServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +12,11 @@ import java.util.List;
 @Controller
 public class CountryController {
 
-    private CountryService countryService;
+    private CountryServiceImpl countryServiceImpl;
+
+    public CountryController(CountryServiceImpl countryServiceImpl) {
+        this.countryServiceImpl = countryServiceImpl;
+    }
 
     @GetMapping("/countries")
     public String getCountries(){
@@ -20,15 +24,15 @@ public class CountryController {
     }
 
     @GetMapping("/allcountries")
-        public List<Country> getAllCountries(Model model){
-        List<Country> countryList = countryService.getAllCountries();
+        public String getAllCountries(Model model){
+        List<Country> countryList = countryServiceImpl.getAllCountries();
         model.addAttribute("countries", countryList);
-        return countryList;
+        return "country";
     }
 
     @PostMapping("/countries/addnew")
     public String addNew(Country country){
-        countryService.addNewCountry(country);
+        countryServiceImpl.addNewCountry(country);
         return "redirect:/countries";
     }
 
