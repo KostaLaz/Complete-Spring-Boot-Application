c904b37eb975f5b5820b24a626897d3c301b620e
kosta <kosta.lazarevski89@gmail.com>
2020-08-10 17:10:51 +0200

    -Location Service

--- src/main/java/com/demo/fleetapp/service/LocationService.java
+++ src/main/java/com/demo/fleetapp/service/LocationService.java
@@ -1,4 +1,19 @@
 package com.demo.fleetapp.service;
 
+import com.demo.fleetapp.model.Location;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface LocationService {
+
+    public List<Location> getAlllocations();
+
+
+    public void addNewlocation(Location location);
+
+    public void deletelocation(Long id);
+
+    public Optional<Location> findById(Long id);
+
 }
--- src/main/java/com/demo/fleetapp/service/LocationServiceImpl.java
+++ src/main/java/com/demo/fleetapp/service/LocationServiceImpl.java
@@ -1,7 +1,40 @@
 package com.demo.fleetapp.service;
 
+import com.demo.fleetapp.model.Location;
+import com.demo.fleetapp.repository.LocationRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class LocationServiceImpl {
+public class LocationServiceImpl implements LocationService {
+
+    private final LocationRepository locationRepository;
+
+    public LocationServiceImpl(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
+
+
+    @Override
+    public List<Location> getAlllocations(){
+        return locationRepository.findAll();
+    }
+
+    @Override
+    public void addNewlocation(Location location){
+        locationRepository.save(location);
+    }
+
+    @Override
+    public void deletelocation(Long id) {
+        locationRepository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Location> findById(Long id) {
+        return locationRepository.findById(id);
+    }
+
 }
