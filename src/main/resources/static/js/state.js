331ef07610acd7bcbd1936f2068c7b9159e55284
kosta <kosta.lazarevski89@gmail.com>
2020-08-10 13:40:41 +0200

    dropdown functionality added

--- src/main/resources/static/js/state.js
+++ src/main/resources/static/js/state.js
@@ -5,15 +5,30 @@ $('document').ready(function(){
         let href = $(this).attr('href');
         $.get(href, function(state, status){
             $('#idEdit').val(state.id);
-            $('#addCo').val(state.id);
-            $('#descriptionEdit').val(state.name);
+            $('#ddlCountryEdit').val(state.countryid);
+            $('#nameEdit').val(state.name);
             $('#capitalEdit').val(state.capital);
             $('#codeEdit').val(state.code);
         });
         $('#editModal').modal();
     });
 
-    $('table #delete-button').on('click', function (event){
+    $('.table #details-button').on('click', function(event){
+        event.preventDefault();
+        let href = $(this).attr('href');
+        $.get(href, function(state, status){
+            $('#idDetails').val(state.id);
+            $('#ddlCountryDetails').val(state.countryid);
+            $('#nameDetails').val(state.name);
+            $('#lastModifiedByDetails').val(state.lastModifiedBy);
+            $('#lastModifiedDateDetails').val(state.lastModifiedDate);
+        });
+        $('#editModal').modal();
+    });
+
+
+
+    $('.table #delete-button').on('click', function (event){
         event.preventDefault();
         let href = $(this).attr('href');
         $('#confirmDeleteButton').attr('href', href);
